d8858abff4cdc199e2426bda4d6efaa95ab9907e
artoftadashi <samuelolanrewaju05@gmail.com>
2024-11-07 10:34:06 +0000

    added an array file for projects. used map function to add them to the list of projects

--- src/components/RecentWork.jsx
+++ src/components/RecentWork.jsx
@@ -1,4 +1,5 @@
 import RecentWorksCard from "./RecentWorksCard";
+import recentWorkContent from "./RecentWorkContent";
 
 export default function RecentWork() {
   return (
@@ -6,33 +7,17 @@ export default function RecentWork() {
       <h1 className="text-[3.5rem] sm:text-[4.5rem] md:text-[7rem] lg:text-[8rem] xl:text-[9.5rem] 2xl:text-[10rem] text-[#fafaff] font-bold uppercase  text-center font-[Thunder]">
         Recent work
       </h1>
-      <div className="flex flex-wrap justify-center gap-16 mt-16 lg:mt-32 ">
-        <RecentWorksCard
-          image="./simonSays.png"
-          title="Simon Says"
-          description="ui development, Front-End engineering"
-        />
-        <RecentWorksCard
-          image="./keeper.png"
-          title="Keeper App"
-          description="ui development, backend engineering"
-        />
-        <RecentWorksCard
-          image="./check-it.png"
-          title="Check-It App"
-          description="ui development, backend engineering"
-        />
-        <RecentWorksCard
-          image="./journal.png"
-          title="Journal App"
-          description="ui development, backend engineering"
-        />
-        <RecentWorksCard
-          image="./secrets.png"
-          title="Secrets App"
-          description="ui development, backend engineering"
-        />
-      </div>
+      <ul className="flex flex-wrap justify-center gap-16 mt-16 lg:mt-32 ">
+        {recentWorkContent.map((content) => (
+          <li key={content.id}>
+            <RecentWorksCard
+              image={content.image}
+              title={content.title}
+              description={content.description}
+            />
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
---
+++ src/components/RecentWorkContent.jsx
@@ -0,0 +1,34 @@
+const recentWorkContent = [
+  {
+    id: 0,
+    image: "./simonSays.png",
+    title: "Simon Says",
+    description: "ui development, Front-End engineering",
+  },
+  {
+    id: 1,
+    image: "./keeper.png",
+    title: "Keeper App",
+    description: "ui development, backend engineering",
+  },
+  {
+    id: 2,
+    image: "./check-it.png",
+    title: "Check-It App",
+    description: "ui development, backend engineering",
+  },
+  {
+    id: 3,
+    image: "./journal.png",
+    title: "Journal App",
+    description: "ui development, backend engineering",
+  },
+  {
+    id: 4,
+    image: "./secrets.png",
+    title: "Secrets App",
+    description: "ui development, backend engineering",
+  },
+];
+
+export default recentWorkContent;
