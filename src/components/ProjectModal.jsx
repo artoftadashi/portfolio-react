992f27c4af1e40d7fc7b20b545676a051aaefe6c
artoftadashi <samuelolanrewaju05@gmail.com>
2024-11-07 16:27:04 +0000

    added glow effect on the modal for mobile

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -4,25 +4,27 @@ import { motion } from "framer-motion";
 export default function ProjectModal({ project, onClose }) {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
+  const spotlightRadius = 500;
+
   const handleMouseMove = (event) => {
     setMousePosition({
-      x: event.clientX - 250,
-      y: event.clientY - 120,
+      x: event.clientX - spotlightRadius / 2,
+      y: event.clientY - spotlightRadius / 2,
     });
   };
 
   if (!project) return null;
 
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.5)] backdrop-blur-sm flex flex-col justify-center items-center  gap-4 z-10 ">
-      <button onClick={onClose}>
+    <div className="fixed top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.5)] backdrop-blur-sm flex flex-col justify-center items-center  gap-4 z-10 overflow-hidden ">
+      <button onClick={onClose} className="animate-jump-in">
         <img
           src="./close-btn.svg"
           className="scale-[.7] hover:scale-[.8] duration-150"
         />
       </button>
       <motion.div
-        className="bg-[#050505] p-8 md:p-16 w-4/5 lg:w-2/3 rounded-2xl z-10 modal-content "
+        className="bg-[#050505] p-8 md:p-16 w-4/5 lg:w-2/3 rounded-2xl z-10 modal-content overflow-hidden  "
         onMouseMove={handleMouseMove}
         style={{
           "--mouse-x": `${mousePosition.x}px`,
