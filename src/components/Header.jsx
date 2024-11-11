eb9893697c842297a1d23cd940a9cc5d11ffc1f2
artoftadashi <samuelolanrewaju05@gmail.com>
2024-11-11 15:29:17 +0000

    changed delay from 1000 to 700

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="flex items-center justify-between mt-8 mx-7 md:mx-10 lg:mx-14 mb-16 z-50 animate-flip-down animate-delay-1000 ">
+    <header className="flex items-center justify-between mt-8 mx-7 md:mx-10 lg:mx-14 mb-16 z-50 animate-flip-down animate-delay-700 ">
       <div className="flex items-center py-1 px-4 bg-[#87FF65] rounded-3xl w-fit">
         <p className="font-bold font-innerTight text-md lg:text-xl sm:text-lg italic text-center">
           {"</s>"}
