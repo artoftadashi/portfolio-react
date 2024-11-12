bd243eb14bb205a33e78844410381fd856b30a51
artoftadashi <samuelolanrewaju05@gmail.com>
2024-11-12 12:17:28 +0000

    made about animations only show on lg devices

--- src/components/aboutComponents/AboutBody.jsx
+++ src/components/aboutComponents/AboutBody.jsx
@@ -14,19 +14,28 @@ export default function ArtBody() {
 
   return (
     <>
-      {showText ? (
-        <div className="w-screen h-screen flex justify-center items-center m-auto overflow-hidden">
-          <h1 className="video-text-2 animate-scale-fade uppercase font-[Thunder] font-bold">
-            Samuel
-          </h1>
-        </div>
-      ) : (
+      <div className="hidden lg:block">
+        {showText ? (
+          <div className="w-screen h-screen flex justify-center items-center m-auto overflow-hidden">
+            <h1 className="video-text-2 animate-scale-fade uppercase font-[Thunder] font-bold">
+              Samuel
+            </h1>
+          </div>
+        ) : (
+          <div className=" animate-fade ">
+            {/* actual content */}
+            <h2>About Section</h2>
+            <p>This is where my about section content goes.</p>
+          </div>
+        )}
+      </div>
+      <div className="block lg:hidden">
         <div className=" animate-fade ">
           {/* actual content */}
           <h2>About Section</h2>
           <p>This is where my about section content goes.</p>
         </div>
-      )}
+      </div>
     </>
   );
 }
