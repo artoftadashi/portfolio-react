b09b22a89261b68ed7f6f7e9f6884a5eafee3666
artoftadashi <samuelolanrewaju05@gmail.com>
2024-11-13 23:50:45 +0000

    edited about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,9 +11,9 @@ export default function About() {
       <p className="lg:w-[75%] mt-10 md:text-xl lg:text-2xl font-innerTight leading-7 lg:leading-9">
         Freelance web developer by day, digital artist by night, and a world
         traveler in spirit (even if two countries are all I've got). Grumpy when
-        hungry, but there's nothing quite like the thrill of fixing a bug at
-        3am. Just don't expect me to be awake long after—sleep is calling, but
-        the bug must die first!
+        hungry, but nothing beats the thrill of fixing a bug at 3am. If I’m not
+        hunting bugs, I’m dropping into Apex Legends, chasing wins and
+        perfecting my aim—sleep can wait!
       </p>
       <a href="/about" className="flex items-center gap-4 mt-20 group w-fit ">
         <p className="text-2xl lg:text-3xl font-innerTight group-hover:underline">
