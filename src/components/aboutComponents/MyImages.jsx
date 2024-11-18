73e22214190d17d93b6bde8a9d05f6b6ef8ed735
artoftadashi <samuelolanrewaju05@gmail.com>
2024-11-18 10:49:33 +0000

    updated pictures

--- src/components/aboutComponents/MyImages.jsx
+++ src/components/aboutComponents/MyImages.jsx
@@ -1,7 +1,7 @@
 const images = [
   "./aboutImages/sam-1.png",
   "./aboutImages/sam-2.png",
-  "./aboutImages/sam-3.png",
+  "./aboutImages/sam-6.png",
   "./aboutImages/sam-4.png",
   "./aboutImages/sam-5.png",
 ];
